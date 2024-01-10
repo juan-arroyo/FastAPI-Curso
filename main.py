5081a5d742818826e7464562d371badfba3a5124
Juan Arroyo <juanmnarroyo@gmail.com>
2024-01-10 13:53:20 +0100

    creando nuevas rutas con parámetros de ruta y de consulta

--- main.py
+++ main.py
@@ -12,5 +12,15 @@ def read_root():
 
 
 @app.get('/hola')
-def hola_mund():
+def hola_mundo():
     return {'Hola': 'Mundo'}
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {'item_id': item_id, 'q': q}
+
+
+@app.get('/calculadora')
+def calcular(operando1: float, operando2: float):
+    return {'suma': operando1 + operando2}
