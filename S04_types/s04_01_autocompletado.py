281e4bc4168cb78f343e701a50bab037286d52c0
Juan Manuel Arroyo <juanmnarroyo@gmail.com>
2024-01-11 13:39:30 +0100

    Probando la función. Errores generados

--- S04_types/s04_01_autocompletado.py
+++ S04_types/s04_01_autocompletado.py
@@ -2,3 +2,6 @@ def get_full_name(first_name, last_name):
     full_name = first_name.title() + ' ' + last_name.title()
     return full_name
 
+
+print(get_full_name('john', 'ortiz'))
+# print(get_full_name('john', 0)) # AttributeError
